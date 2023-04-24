22a3ec1757e13483bd51b63e092bd5c8eae2e277
Marina120803 <petrenkomarina567@gmail.com>
2023-04-24 11:07:06 +0300

    Добавьте файлы проекта.

---
+++ UDPSocketChat/Program.cs
@@ -0,0 +1,12 @@
+﻿using UDPSocketChat;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        UDPChat chat = new UDPChat();
+        chat.handlerUDPChat();
+
+        Console.WriteLine("Hello, World!");
+    }
+}
